014b0f06da7736e5594eea7103fba33944fd06bc
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-06 00:35:26 +0200

    [Understanding the basics](<>): Sending response

--- javascript-refresher/source-code/app.js
+++ javascript-refresher/source-code/app.js
@@ -3,6 +3,11 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write('<html lang="en">');
+  res.write("<head><title>First page</title></head>");
+  res.write("<body><h1>Hello from node.js server!</h1></body>");
+  res.write("</html>");
 });
 
 server.listen(3100);
