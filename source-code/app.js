75718919059c3888996ded620ebb0ad1c122bcc5
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-14 10:15:58 +0200

    [Working with Express.js](<>): Limiting middleware to execution POST request

--- source-code/app.js
+++ source-code/app.js
@@ -15,7 +15,8 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+// Limit to execute a POST request
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
