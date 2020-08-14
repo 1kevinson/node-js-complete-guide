c375fdb46dc88119e406b2cea05ca067a8d9f3a9
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-14 09:39:57 +0200

    [Working with Express.js](<next>): Adding middleware

--- source-code/app.js
+++ source-code/app.js
@@ -3,6 +3,17 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+
+// Enter in a middleware
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next(); //Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("in another middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3100);
