232affb12dea924071e8abe2fc28202fe9ea2e9f
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-14 10:00:34 +0200

    [Working with Express.js](<next>): Handling different routes

--- source-code/app.js
+++ source-code/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 
 const app = express();
 
-// Enter in a middleware
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next(); //Allows the request to continue to the next middleware in line
+//Request goes from the top to bottom
+
+app.use("/", (req, res, next) => {
+  console.log("always works");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add", (req, res, next) => {
+  console.log("in middleware");
+  res.send("<h1>The 'Add page'</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("in another middleware");
   res.send("<h1>Hello from express</h1>");
 });
