e54798ea53d8e9b5a773640029e847106587a153
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-14 13:06:03 +0200

    [Working with Express.js](<mainModule>): Using a helper function for navigation

--- source-code/routes/shop.js
+++ source-code/routes/shop.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
+const rootDir = require("../Util/path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // __dirname is absolute path of the operating system to the project folder
+  res.sendFile(path.join(rootDir, "views", "shop.html")); // __dirname is absolute path of the operating system to the project folder
 });
 
 module.exports = router;
